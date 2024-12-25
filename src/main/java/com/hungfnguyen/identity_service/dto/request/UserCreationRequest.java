d59feefaafafdd8c4b6532154d73bb3545593c33
hungfnguyen <hungfnguyen.de@gmail.com>
2024-12-25 22:02:26 +0700

    exception and validation

--- src/main/java/com/hungfnguyen/identity_service/dto/request/UserCreationRequest.java
+++ src/main/java/com/hungfnguyen/identity_service/dto/request/UserCreationRequest.java
@@ -1,10 +1,14 @@
 package com.hungfnguyen.identity_service.dto.request;
 
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class UserCreationRequest {
+    @Size(min = 3, message = "Username must be at least 3 characters")
     private String userName;
+    @Size(min = 8, message  = "Password must be at least 8 characters")
     private String password;
     private String firstName;
     private String lastName;
